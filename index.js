d4f21e9acff3358f11f6b5c55e9aaa1e956b9b4b
Rishen <rishenlap@gmail.com>
2025-03-09 21:26:00 +0545

    first task done before connecting to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
+
 app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
+});
+app.post('/api/users', (req,res)=>{
+  res.json({"username": req.body.username});
 });
 
 
