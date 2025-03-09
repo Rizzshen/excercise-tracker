a8a264777851e03468713f652f1cae5799e04bd5
Rishen <rishenlap@gmail.com>
2025-03-09 22:34:35 +0545

    MongoDB set up and used to post username and id

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log("MongoDB connected");
+let Person;
+const personSchema = new mongoose.Schema({
+  name: {type: String,required: true}
+  
+});
+Person = mongoose.model('Person', personSchema);
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,8 +19,11 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-app.post('/api/users', (req,res)=>{
-  res.json({"username": req.body.username});
+app.post('/api/users', async (req,res)=>{
+  let user = new Person({name: req.body.username});
+  let data = await user.save();
+  console.log(data);
+  res.json({"username": data.name, "id": data._id});
 });
 
 
